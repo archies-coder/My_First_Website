bb2007c3759a56099a1cff1732d33d066faee9de
archies-coder <35570411+archies-coder@users.noreply.github.com>
2018-01-25 12:25:31 +0530

    Add files via upload

---
+++ effects.js
@@ -0,0 +1,33 @@
+// project2index.html
+
+// var userName= prompt("Enter your name ");
+var welcomeMessage=	document.getElementById("try");
+var styleDiv= document.getElementsByClassName("javascript_started");
+var trollMsg= document.getElementsByClassName("first_button");
+
+
+
+function submitClick(){
+	trollMsg.addEventListner("click", alert("you have been trolled...Don't expect too much off my first site") )
+}
+
+
+
+
+// welcomeMessage.innerHTML +="Hi, Welcome "+ userName;
+// welcomeMessage.style.fontSize="25px";
+styleDiv[0].style.display="inline";
+styleDiv[0].style.float="left";
+styleDiv[0].style.marginLeft="20px";
+welcomeMessage.style.letterSpacing="1px";
+styleDiv[0].style.color="#EF0D0D";
+
+
+console.log("hey there");
+console.log("2nd message")
+
+
+// about.html
+
+// fixed navigation menu
+
